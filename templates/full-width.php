49e8cd1c766a891d8f0d2eada67eeae8c00dddd4
itvn9online <itvn9online@gmail.com>
2017-09-14 22:36:27 +0700

    Initial commit

--- templates/full-width.php
+++ templates/full-width.php
@@ -2,6 +2,20 @@
 /*
 Template Name: Landing page
 */
-$act = basename(__FILE__, '.php');
+$act = basename(__FILE__, '.php'); // do not remove this code
 
-include_once EB_THEME_URL . 'index.php';
+//
+include EB_THEME_PLUGIN_INDEX . 'global/page_templates_top.php'; // do not remove this code
+
+/*
+* BEGIN Custom code
+*/
+?>
+<h1><?php echo $post->post_title; ?></h1>
+<?php
+/*
+* END Custom code
+*/
+
+//
+include EB_THEME_PLUGIN_INDEX . 'global/page_templates_footer.php'; // do not remove this code

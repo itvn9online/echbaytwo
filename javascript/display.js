1bbc380014c72494765965deff9569c9ac45796f
itvn9online <itvn9online@gmail.com>
2017-10-04 04:16:48 +0700

    fixed english

--- javascript/display.js
+++ javascript/display.js
@@ -5,7 +5,7 @@
 /*
 * Các function riêng cho từng theme -> cần chạy cái nào thì định nghĩa cái đó ra
 */
-if ( cf_tester_mode == 1 ) console.log('Code your custom javascript to hera');
+if ( cf_tester_mode == 1 ) console.log('Code your custom javascript to here');
 
 
 

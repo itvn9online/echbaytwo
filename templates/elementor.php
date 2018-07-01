f23f69b10e07b0710a73ae2a92033f491097f46c
itvn9online <itvn9online@gmail.com>
2018-07-01 07:51:28 +0700

    page for elementor

---
+++ templates/elementor.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: For Elementor
+Description: Page template for Elementor Page Builder plugin
+*/
+$act = basename(__FILE__, '.php'); // do not remove this code
+
+//
+include EB_THEME_PLUGIN_INDEX . 'global/page_templates_top.php'; // do not remove this code
+
+/*
+* BEGIN Custom code
+*/
+echo the_content();
+/*
+* END Custom code
+*/
+
+//
+include EB_THEME_PLUGIN_INDEX . 'global/page_templates_footer.php'; // do not remove this code

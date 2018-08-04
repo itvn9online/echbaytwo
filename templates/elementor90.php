d0cc1c6bc569f91834c590f4adda4d6435471dbb
itvn9online <itvn9online@gmail.com>
2018-08-04 14:11:28 +0700

    up to 1.0.1

--- templates/elementor90.php
+++ templates/elementor90.php
@@ -1,9 +1,12 @@
 <?php
 /*
-Template Name: For Elementor
-Description: Page template for Elementor Page Builder plugin (full size)
+Template Name: For Elementor 90%
+Description: Page template for Elementor Page Builder plugin (width 90%, set max-width by class CSS w90)
 */
 $act = basename(__FILE__, '.php'); // do not remove this code
+
+// fixed size for page
+$__cf_row['cf_custom_page_width_main'] = 'w90'; // do not remove this code
 
 //
 include EB_THEME_PLUGIN_INDEX . 'global/page_templates_top.php'; // do not remove this code
--- templates/elementor99.php
+++ templates/elementor99.php
@@ -1,9 +1,12 @@
 <?php
 /*
-Template Name: For Elementor
-Description: Page template for Elementor Page Builder plugin (full size)
+Template Name: For Elementor 1024px
+Description: Page template for Elementor Page Builder plugin (width and max-width 999px)
 */
 $act = basename(__FILE__, '.php'); // do not remove this code
+
+// fixed size for page
+$__cf_row['cf_custom_page_width_main'] = 'w99'; // do not remove this code
 
 //
 include EB_THEME_PLUGIN_INDEX . 'global/page_templates_top.php'; // do not remove this code

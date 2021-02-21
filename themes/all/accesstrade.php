2a2fcbc639224ffba3843ed0c0a87c230bbaceaf
Dao Quoc Dai <itvn9online@gmail.com>
2021-02-21 22:50:30 +0700

    Create accesstrade.php

---
+++ themes/all/accesstrade.php
@@ -0,0 +1,34 @@
+<?php 
+/*
+* Save from Sat, 04 Nov 2017 10:35:47 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36
+*/
+$eb_all_themes_support["accesstrade"] = array();
+$eb_all_themes_support["accesstrade"]["name"] = "accesstrade";
+$eb_all_themes_support["accesstrade"]["screenshot"] = "https://img1.webgiare.org/accesstrade.jpg";
+$eb_all_themes_support["accesstrade"]["author"] = "";
+$eb_all_themes_support["accesstrade"]["demo"] = "";
+$eb_all_themes_support["accesstrade"]["price"] = 8000000;
+//
+$eb_all_themes_support["accesstrade"]["top"][] = "accesstrade-top1.php";
+//$eb_all_themes_support["accesstrade"]["top"][] = "accesstrade-top2.php";
+//$eb_all_themes_support["accesstrade"]["top"][] = "accesstrade-top3.php";
+$eb_all_themes_support["accesstrade"]["top"][] = "top_widget.php";
+$eb_all_themes_support["accesstrade"]["top"][] = "bigbanner-top1.php";
+$eb_all_themes_support["accesstrade"]["top"][] = "breadcrumb-top1.php";
+//
+$eb_all_themes_support["accesstrade"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["accesstrade"]["footer"][] = "accesstrade-footer1.php";
+//$eb_all_themes_support["accesstrade"]["footer"][] = "accesstrade-footer2.php";
+//
+//$eb_all_themes_support["accesstrade"]["home"][] = "echbaytwo-home1.php";
+//$eb_all_themes_support["accesstrade"]["home"][] = "echbaytwo-home3.php";
+$eb_all_themes_support["accesstrade"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["accesstrade"]["threaddetails"][] = "accesstrade-threaddetails.html";
+//
+$eb_all_themes_support["accesstrade"]["threadnode"][] = "accesstrade-threadnode.html";
+//$eb_all_themes_support["accesstrade"]["threadnode"][] = "parina-threadnode.html";

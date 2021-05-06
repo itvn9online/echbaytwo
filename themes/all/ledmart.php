87f9c7fd79d3dd232457e36b12b4cf0f90d43b88
Dao Quoc Dai <itvn9online@gmail.com>
2021-05-06 09:25:30 +0700

    new theme

---
+++ themes/all/ledmart.php
@@ -0,0 +1,43 @@
+<?php 
+/*
+* Save from Mon, 03 May 2021 07:30:14 +0700
+* By itvn9online@yahoo.com #1
+* From IP: ::1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36
+*/
+$eb_all_themes_name = 'ledmart';
+$eb_all_themes_support[$eb_all_themes_name] = array();
+$eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
+$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/ledmart.jpg";
+$eb_all_themes_support[$eb_all_themes_name]["author"] = "";
+$eb_all_themes_support[$eb_all_themes_name]["demo"] = "";
+$eb_all_themes_support[$eb_all_themes_name]["price"] = 4500000;
+//
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "ledmart-top1.php";
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
+// Banner tràn màn hình
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
+// Banner bo gọn trong khung
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbannerFixed-top1.php";
+// breadcrumb tràn màn hình
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb-top1.php";
+// breadcrumb gọn trong khung
+$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2-top1.php";
+// breadcrumb tràn màn hình (kèm nền mặc định)
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumbbg-top1.php";
+// breadcrumb gọn trong khung (kèm nền mặc định)
+//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2bg-top1.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "ledmart-footer1.php";
+// Logo đối tác
+//$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "doitac-footer1.php";
+$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
+//
+//$eb_all_themes_support[$eb_all_themes_name]["home"][] = "ledmart-home1.php";
+$eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "ledmart-threaddetails.html";
+//
+$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "ledmart-threadnode.html";

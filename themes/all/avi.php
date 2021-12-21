e4e21775984bf58d685b9a1e95f30cefcd841d31
itvn9online <itvn9online@gmail.com>
2021-12-21 19:11:22 +0700

    sync

--- themes/all/avi.php
+++ themes/all/avi.php
@@ -6,38 +6,33 @@
 * On domain: localhost:8888
 * User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36
 */
-$eb_all_themes_name = 'avi';
-$eb_all_themes_support[$eb_all_themes_name] = array();
-$eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
-$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/avi.jpg";
-$eb_all_themes_support[$eb_all_themes_name]["author"] = "";
-$eb_all_themes_support[$eb_all_themes_name]["demo"] = "";
-$eb_all_themes_support[$eb_all_themes_name]["price"] = 4500000;
+$eb_all_themes_name = basename( __FILE__, '.php' );
+$eb_all_themes_support[ $eb_all_themes_name ] = array();
+$eb_all_themes_support[ $eb_all_themes_name ][ "name" ] = $eb_all_themes_name;
+$eb_all_themes_support[ $eb_all_themes_name ][ "screenshot" ] = "https://img1.webgiare.org/$eb_all_themes_name.jpg";
+$eb_all_themes_support[ $eb_all_themes_name ][ "author" ] = "";
+$eb_all_themes_support[ $eb_all_themes_name ][ "demo" ] = "themes.webgiare.org/$eb_all_themes_name";
+$eb_all_themes_support[ $eb_all_themes_name ][ "tags" ] = "ladingpage";
+$eb_all_themes_support[ $eb_all_themes_name ][ "price" ] = 80000;
 //
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "avi-top1.php";
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "top" ][] = "$eb_all_themes_name-top1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "top" ][] = "top_widget.php";
 // Banner tràn màn hình
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "top" ][] = "bigbanner-top1.php";
 // Banner bo gọn trong khung
 //$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbannerFixed-top1.php";
 // breadcrumb tràn màn hình
 //$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb-top1.php";
 // breadcrumb gọn trong khung
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2-top1.php";
-// breadcrumb tràn màn hình (kèm nền mặc định)
-//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumbbg-top1.php";
-// breadcrumb gọn trong khung (kèm nền mặc định)
-//$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2bg-top1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "top" ][] = "breadcrumb2-top1.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
-$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "avi-footer1.php";
-// Logo đối tác
-//$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "doitac-footer1.php";
-$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "footer" ][] = "footer_widget.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "footer" ][] = "$eb_all_themes_name-footer1.php";
+//$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["home"][] = "avi-home1.php";
-$eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "home" ][] = "$eb_all_themes_name-home1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "home" ][] = "home_widget.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "avi-threaddetails.html";
+$eb_all_themes_support[ $eb_all_themes_name ][ "threaddetails" ][] = "$eb_all_themes_name-threaddetails.html";
 //
-$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "avi-threadnode.html";
+$eb_all_themes_support[ $eb_all_themes_name ][ "threadnode" ][] = "$eb_all_themes_name-threadnode.html";
--- themes/all/bei.php
+++ themes/all/bei.php
@@ -1,38 +1,38 @@
-<?php 
+<?php
 /*
-* Save from Mon, 09 Jul 2018 10:40:04 +0700
-* By itvn9online@yahoo.com #1
-* From IP: 127.0.0.1
-* On domain: localhost:8888
-* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36
-*/
-$eb_all_themes_name = 'bei';
-$eb_all_themes_support[$eb_all_themes_name] = array();
-$eb_all_themes_support[$eb_all_themes_name]["name"] = $eb_all_themes_name;
-$eb_all_themes_support[$eb_all_themes_name]["screenshot"] = "https://img1.webgiare.org/bei.jpg";
-$eb_all_themes_support[$eb_all_themes_name]["author"] = "";
-$eb_all_themes_support[$eb_all_themes_name]["demo"] = "themes.webgiare.org/bei";
-$eb_all_themes_support[$eb_all_themes_name]["tags"] = "ladingpage";
-$eb_all_themes_support[$eb_all_themes_name]["price"] = 80000;
+ * Save from Mon, 09 Jul 2018 10:40:04 +0700
+ * By itvn9online@yahoo.com #1
+ * From IP: 127.0.0.1
+ * On domain: localhost:8888
+ * User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36
+ */
+$eb_all_themes_name = basename( __FILE__, '.php' );
+$eb_all_themes_support[ $eb_all_themes_name ] = array();
+$eb_all_themes_support[ $eb_all_themes_name ][ "name" ] = $eb_all_themes_name;
+$eb_all_themes_support[ $eb_all_themes_name ][ "screenshot" ] = "https://img1.webgiare.org/$eb_all_themes_name.jpg";
+$eb_all_themes_support[ $eb_all_themes_name ][ "author" ] = "";
+$eb_all_themes_support[ $eb_all_themes_name ][ "demo" ] = "themes.webgiare.org/$eb_all_themes_name";
+$eb_all_themes_support[ $eb_all_themes_name ][ "tags" ] = "ladingpage";
+$eb_all_themes_support[ $eb_all_themes_name ][ "price" ] = 80000;
 //
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bei-top1.php";
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "top_widget.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "top" ][] = "$eb_all_themes_name-top1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "top" ][] = "top_widget.php";
 // Banner tràn màn hình
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbanner-top1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "top" ][] = "bigbanner-top1.php";
 // Banner bo gọn trong khung
 //$eb_all_themes_support[$eb_all_themes_name]["top"][] = "bigbannerFixed-top1.php";
 // breadcrumb tràn màn hình
 //$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb-top1.php";
 // breadcrumb gọn trong khung
-$eb_all_themes_support[$eb_all_themes_name]["top"][] = "breadcrumb2-top1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "top" ][] = "breadcrumb2-top1.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "footer_widget.php";
-$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "bei-footer1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "footer" ][] = "footer_widget.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "footer" ][] = "$eb_all_themes_name-footer1.php";
 //$eb_all_themes_support[$eb_all_themes_name]["footer"][] = "echbaytwo-footer3.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["home"][] = "bei-home1.php";
-$eb_all_themes_support[$eb_all_themes_name]["home"][] = "home_widget.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "home" ][] = "$eb_all_themes_name-home1.php";
+$eb_all_themes_support[ $eb_all_themes_name ][ "home" ][] = "home_widget.php";
 //
-$eb_all_themes_support[$eb_all_themes_name]["threaddetails"][] = "bei-threaddetails.html";
+$eb_all_themes_support[ $eb_all_themes_name ][ "threaddetails" ][] = "$eb_all_themes_name-threaddetails.html";
 //
-$eb_all_themes_support[$eb_all_themes_name]["threadnode"][] = "bei-threadnode.html";
+$eb_all_themes_support[ $eb_all_themes_name ][ "threadnode" ][] = "$eb_all_themes_name-threadnode.html";

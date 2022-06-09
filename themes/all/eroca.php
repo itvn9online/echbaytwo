78da89fe1daea24150b8c1cb05c803b3701a7d30
itvn9online <itvn9online@gmail.com>
2022-06-09 12:59:12 +0700

    Create eroca.php

---
+++ themes/all/eroca.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+* Save from Sat, 04 Nov 2017 10:35:47 +0700
+* By itvn9online@yahoo.com #1
+* From IP: 127.0.0.1
+* On domain: localhost:8888
+* User agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36
+*/
+$eb_all_themes_support["eroca"] = array();
+$eb_all_themes_support["eroca"]["name"] = "eroca";
+$eb_all_themes_support["eroca"]["screenshot"] = "https://img1.webgiare.org/eroca.jpg";
+$eb_all_themes_support["eroca"]["author"] = "";
+$eb_all_themes_support["eroca"]["demo"] = "";
+$eb_all_themes_support["eroca"]["price"] = 8000000;
+//
+$eb_all_themes_support["eroca"]["top"][] = "eroca-top1.php";
+//$eb_all_themes_support["eroca"]["top"][] = "eroca-top2.php";
+//$eb_all_themes_support["eroca"]["top"][] = "eroca-top3.php";
+$eb_all_themes_support["eroca"]["top"][] = "top_widget.php";
+$eb_all_themes_support["eroca"]["top"][] = "bigbanner-top1.php";
+$eb_all_themes_support["eroca"]["top"][] = "breadcrumb-top1.php";
+//
+$eb_all_themes_support["eroca"]["footer"][] = "footer_widget.php";
+$eb_all_themes_support["eroca"]["footer"][] = "eroca-footer1.php";
+//$eb_all_themes_support["eroca"]["footer"][] = "eroca-footer2.php";
+//
+//$eb_all_themes_support["eroca"]["home"][] = "echbaytwo-home1.php";
+//$eb_all_themes_support["eroca"]["home"][] = "echbaytwo-home3.php";
+$eb_all_themes_support["eroca"]["home"][] = "home_widget.php";
+//
+$eb_all_themes_support["eroca"]["threaddetails"][] = "eroca-threaddetails.html";
+//
+$eb_all_themes_support["eroca"]["threadnode"][] = "eroca-threadnode.html";
